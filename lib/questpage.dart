0bdce98bd84c127d6d63029bdd65b2f7aad50b5a
Adjeko <adjeko88@gmail.com>
2020-03-21 14:45:52 +0100

    created quest detail page with navigation from quest page to it

--- lib/questpage.dart
+++ lib/questpage.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'questdetailpage.dart';
 
 class QuestPage extends StatelessWidget {
+
+  List<String> mockList = ["Test1", "Aufgabe", "Hände waschen"];
 
   @override
   Widget build(BuildContext context) {
     return 
-    Icon(Icons.directions_bike);
+    ListView.builder(
+      itemCount: mockList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => QuestDetailPage(content: mockList[index],)
+              ),
+            );
+          },
+          title: Text(mockList[index]),
+        );
+      }
+    );
   }
 }
