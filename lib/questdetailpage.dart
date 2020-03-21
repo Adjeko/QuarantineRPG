58e31cc310e792c023316881b706a578144f2bea
Adjeko <adjeko88@gmail.com>
2020-03-21 22:00:03 +0100

    all data in quest detail page

--- lib/questdetailpage.dart
+++ lib/questdetailpage.dart
@@ -17,46 +17,91 @@ class QuestDetailPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text(quest['description']),
+          Container(
+            width: 200,
+            height: 200,
+            child: FlareActor(
+              'assets/${quest['icon']}.flr',
+              alignment: Alignment.center,
+              // fit: BoxFit.contain,
+            ),
+          ),
+          Center(
+            child: Text(quest['title']),
+          ),
+          Center(
+            child: Text(quest['description']),
+          ),
           FutureBuilder(
             future: quest['reward'].get(),
             builder: (context, rewardsSnap) {
               if(rewardsSnap.connectionState == ConnectionState.done) {
                 if(rewardsSnap.hasData){
-                  return Column(
-                    children: [
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: rewardsSnap.data['realItems'].length,
-                        itemBuilder: (context, index) {
-                          return Text(rewardsSnap.data['realItems'][index]);
-                        }
-                      ),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: rewardsSnap.data['digitalItems'].length,
-                        itemBuilder: (context, index) {
-                          return FutureBuilder(
-                            future: rewardsSnap.data['digitalItems'][0].get(),
-                            builder: (context, digitalItemsSnap) {
-                              if(digitalItemsSnap.connectionState == ConnectionState.done) {
-                                if(digitalItemsSnap.hasData){
-                                  return Text(digitalItemsSnap.data['icon'].toString());
-                                } else {
-                                  return Text("Item has no data");
-                                }
-                              } else {
-                                return CircularProgressIndicator();
-                              }
-                            }
-                          );
-                        },
-                      ),
-                    ],
+                  return Center(
+                        child: Text("Erfahrung: ${rewardsSnap.data['experience']}"),
+                      );
+                } else {
+                  return Text("keine String");
+                } 
+              } else {
+                return CircularProgressIndicator();
+              }
+            }
+          ),
+          Center(
+            child: Text("Reale Belohnungen"),
+          ),
+          FutureBuilder(
+            future: quest['reward'].get(),
+            builder: (context, rewardsSnap) {
+              if(rewardsSnap.connectionState == ConnectionState.done) {
+                if(rewardsSnap.hasData){
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: rewardsSnap.data['realItems'].length,
+                    itemBuilder: (context, index) {
+                      return Text(rewardsSnap.data['realItems'][index]);
+                    }
                   );
                 } else {
                   return Text("keine String");
-                }
+                } 
+              } else {
+                return CircularProgressIndicator();
+              }
+            }
+          ),
+          Center(
+            child: Text("Digitale Belohnungen"),
+          ),
+          FutureBuilder(
+            future: quest['reward'].get(),
+            builder: (context, rewardsSnap) {
+              if(rewardsSnap.connectionState == ConnectionState.done) {
+                if(rewardsSnap.hasData){
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: rewardsSnap.data['digitalItems'].length,
+                    itemBuilder: (context, index) {
+                      return FutureBuilder(
+                        future: rewardsSnap.data['digitalItems'][0].get(),
+                        builder: (context, digitalItemsSnap) {
+                          if(digitalItemsSnap.connectionState == ConnectionState.done) {
+                            if(digitalItemsSnap.hasData){
+                              return Text(digitalItemsSnap.data['icon'].toString());
+                            } else {
+                              return Text("Item has no data");
+                            }
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        }
+                      );
+                    },
+                  );
+                } else {
+                  return Text("keine String");
+                } 
               } else {
                 return CircularProgressIndicator();
               }
