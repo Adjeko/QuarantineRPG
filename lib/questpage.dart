e61702f1fb7d996ccb7ea31a1de19ca9a3149444
Adjeko <adjeko88@gmail.com>
2020-03-21 17:16:09 +0100

    Firestore integrated into quest page

--- lib/questpage.dart
+++ lib/questpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'questdetailpage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class QuestPage extends StatelessWidget {
 
@@ -8,22 +9,55 @@ class QuestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-    ListView.builder(
-      itemCount: mockList.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => QuestDetailPage(content: mockList[index],)
-              ),
-            );
+    
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: StreamBuilder(
+          stream: Firestore.instance.collection('tasks').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if(snapshot.hasError)
+              return new Text("Error: ${snapshot.error}");
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return new Text("Loading...");
+              default:
+                return new ListView(
+                  children: 
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                      return new ListTile(
+                        leading: Icon(Icons.album, size:50),
+                        title: Text(document['title']),
+                        subtitle: Text(document['description']),
+                      );
+                    }).toList(),
+                );
+            }
           },
-          title: Text(mockList[index]),
-        );
-      }
+        ),
+      ),
     );
+    
   }
 }
+
+
+
+
+
+// ListView.builder(
+//       itemCount: mockList.length,
+//       itemBuilder: (context, index) {
+//         return ListTile(
+//           onTap: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => QuestDetailPage(content: mockList[index],)
+//               ),
+//             );
+//           },
+//           title: Text(mockList[index]),
+//         );
+//       }
+//     );
