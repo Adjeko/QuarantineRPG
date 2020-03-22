57f0e23bc220e8a9d83423207c15994240d5bf19
Adjeko <adjeko88@gmail.com>
2020-03-22 15:13:32 +0100

    Name and character animation

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quarantäne RPG',
+      title: 'BeInside',
       theme: ThemeData(
         primarySwatch: Colors.lime,
       ),
-      home: MyHomePage(title: 'Quarantäne RPG'),
+      home: MyHomePage(title: 'BeInside'),
     );
   }
 }
